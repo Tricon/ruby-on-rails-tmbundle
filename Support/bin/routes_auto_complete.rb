1ad4056619a1913e44a2a9cf62243ff9b4d8efe6
David Aaron Fendley <tricon@me.com>
2014-12-31 09:06:19 -0600

    Account for current word and additional chars.

--- Support/bin/routes_auto_complete.rb
+++ Support/bin/routes_auto_complete.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 
 tm_dialog = ENV["DIALOG"]
+current_word = STDIN.read
 Dir.chdir(ENV["TM_PROJECT_DIRECTORY"])
 
 routes = %x(spring rake routes).gsub(/^ *([a-z0-9_]+)?.*$/, '\1').gsub(/^\s*$\n/, "").split("\n").map do |route|
   %w(_path).map { |extension| "{ display = #{route.to_s + extension}; }" }
 end
 
-%x(#{tm_dialog} popup --returnChoice --suggestions '( #{routes.flatten.join(", ")} )')
+%x(#{tm_dialog} popup --returnChoice --alreadyTyped '#{current_word}' --caseInsensitive --additionalWordCharacters '_ ' --suggestions '( #{routes.flatten.join(", ")} )')
